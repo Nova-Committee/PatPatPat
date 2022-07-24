7df3ce8f59817e9589c3dc9940d3762e9b011cc1
MikhailTapio <mikhailtapio13468@163.com>
2022-07-24 18:18:17 +0800

    Proxies

--- src/main/scala/committee/nova/examplemod/ExampleMod.scala
+++ src/main/scala/committee/nova/examplemod/ExampleMod.scala
@@ -1,6 +1,8 @@
 package committee.nova.examplemod
 
 import committee.nova.examplemod.common.proxy.CommonProxy
+import cpw.mods.fml.common.Mod.EventHandler
+import cpw.mods.fml.common.event.{FMLInitializationEvent, FMLPostInitializationEvent, FMLPreInitializationEvent}
 import cpw.mods.fml.common.{Mod, SidedProxy}
 
 @Mod(modid = ExampleMod.MODID, useMetadata = true, modLanguage = "scala")
@@ -10,4 +12,13 @@ object ExampleMod {
 
   @SidedProxy(serverSide = pkgPrefix + "common.proxy.CommonProxy", clientSide = pkgPrefix + "client.proxy.ClientProxy")
   var proxy: CommonProxy = _
+
+  @EventHandler
+  def preInit(e: FMLPreInitializationEvent): Unit = proxy.preInit(e)
+
+  @EventHandler
+  def init(e: FMLInitializationEvent): Unit = proxy.init(e)
+
+  @EventHandler
+  def postInit(e: FMLPostInitializationEvent): Unit = proxy.postInit(e)
 }
