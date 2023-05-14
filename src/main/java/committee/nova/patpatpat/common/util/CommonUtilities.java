fed54e41c1c13fa0cfaaa8f65bdd574b8f540b00
MikhailTapio <mikhailtapio13468@163.com>
2023-05-14 14:50:01 +0800

    Allows addPattable

--- src/main/java/committee/nova/patpatpat/common/util/CommonUtilities.java
+++ src/main/java/committee/nova/patpatpat/common/util/CommonUtilities.java
@@ -1,7 +1,6 @@
 package committee.nova.patpatpat.common.util;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.CatEntity;
 import net.minecraft.entity.passive.WolfEntity;
@@ -9,14 +8,16 @@ import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class CommonUtilities {
-    private static final Map<Class<?>, List<SoundEvent>> pattables = ImmutableMap.of(
-            CatEntity.class, ImmutableList.of(SoundEvents.CAT_PURR, SoundEvents.CAT_PURREOW),
-            WolfEntity.class, ImmutableList.of(SoundEvents.WOLF_PANT, SoundEvents.WOLF_WHINE)
-    );
+    private static final Map<Class<? extends Entity>, List<SoundEvent>> pattables = new HashMap<>();
+
+    public static void addPattable(Class<? extends Entity> entityClass, List<SoundEvent> soundEvents) {
+        pattables.put(entityClass, soundEvents);
+    }
 
     public static boolean isPattable(Entity e) {
         if (e == null) return false;
@@ -26,8 +27,13 @@ public class CommonUtilities {
 
     public static List<SoundEvent> getPattedSounds(Entity e) {
         final List<SoundEvent> l = new ArrayList<>();
-        for (final Map.Entry<Class<?>, List<SoundEvent>> t : pattables.entrySet())
+        for (final Map.Entry<Class<? extends Entity>, List<SoundEvent>> t : pattables.entrySet())
             if (t.getKey().isAssignableFrom(e.getClass())) l.addAll(t.getValue());
         return l;
+    }
+
+    static {
+        pattables.put(CatEntity.class, ImmutableList.of(SoundEvents.CAT_PURR, SoundEvents.CAT_PURREOW));
+        pattables.put(WolfEntity.class, ImmutableList.of(SoundEvents.WOLF_PANT, SoundEvents.WOLF_WHINE));
     }
 }
